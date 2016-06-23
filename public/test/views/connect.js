571bae24ace0397fcadafbb803264920b2e36be3
timwis <tim@timwis.com>
2016-06-23 06:24:56 -0400

    connect test uses new field names

--- public/test/views/connect.js
+++ public/test/views/connect.js
@@ -1,6 +1,7 @@
 /* global Event */
 const test = require('tape')
 require('jsdom-global')()
+// const document = require('global/document')
 
 const connect = require('../../src/views/connect')
 
@@ -8,8 +9,8 @@ test('connect view sends form values as payload on submit', (t) => {
   t.plan(1)
   const state = { db: { config: {} } }
   const sample = {
-    hostname: 'localhost',
-    username: 'root',
+    host: 'localhost',
+    user: 'root',
     password: 'lolol',
     database: 'cake'
   }
@@ -17,8 +18,8 @@ test('connect view sends form values as payload on submit', (t) => {
     t.deepEqual(action.payload, sample, 'payload matches form values')
   }
   const tree = connect(null, state, send)
-  tree.querySelector('#hostname').value = sample.hostname
-  tree.querySelector('#username').value = sample.username
+  tree.querySelector('#host').value = sample.host
+  tree.querySelector('#user').value = sample.user
   tree.querySelector('#password').value = sample.password
   tree.querySelector('#database').value = sample.database
   tree.querySelector('form').dispatchEvent(new Event('submit'))
