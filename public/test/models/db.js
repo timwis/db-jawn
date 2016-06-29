ebab1335a43e3638a75f2a3e41e2200aa72abbe5
timwis <tim@timwis.com>
2016-06-29 06:23:57 -0400

    add tests for db model's effects

--- public/test/models/db.js
+++ public/test/models/db.js
@@ -18,3 +18,53 @@ test('db model: reducers: receiveTableDeletion', (t) => {
   const result = db.reducers.receiveTableDeletion(action, state)
   t.equal(result.tables.length, 2)
 })
+
+test('db model: effects: getTableList', (t) => {
+  t.plan(2)
+  const state = {
+    client: {
+      getTables: () => Promise.resolve({
+        rows: [
+          { tablename: 'foo' },
+          { tablename: 'bar' }
+        ]
+      })
+    }
+  }
+  const send = (event, data) => {
+    t.equal(event, 'db:receiveTableList', 'calls correct reducer')
+    t.deepEqual(data.payload, ['foo', 'bar'], 'table names were plucked')
+  }
+  db.effects.getTableList({}, state, send)
+})
+
+test('db model: effects: createTable', (t) => {
+  t.plan(2)
+  const state = {
+    client: {
+      createTable: (name) => Promise.resolve()
+    }
+  }
+  const tableName = 'users'
+  const send = (event, data) => {
+    t.equal(event, 'db:receiveNewTable', 'calls correct reducer')
+    t.equal(data.name, tableName, 'passes on table name')
+  }
+  db.effects.createTable({ name: tableName }, state, send)
+})
+
+test('db model: effects: deleteTable', (t) => {
+  t.plan(2)
+  const state = {
+    tables: ['foo', 'bar', 'baz'],
+    client: {
+      deleteTable: (name) => Promise.resolve()
+    }
+  }
+  const tableName = 'bar'
+  const send = (event, data) => {
+    t.equal(event, 'db:receiveTableDeletion', 'calls correct reducer')
+    t.equal(data.index, 1, 'finds table index')
+  }
+  db.effects.deleteTable({ name: tableName }, state, send)
+})
